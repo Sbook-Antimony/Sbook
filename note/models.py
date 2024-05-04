400eb517e1a8fe01971b4464ed3c3d443ced94f4
ken-morel <engonken8@gmail.com>
2024-05-04 18:38:12 +0100

    Update models.py

--- note/models.py
+++ note/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.db.models import *
 
 # Create your models here.
+
+
+class NoteUser(Model):
+    pass
+    
+class Note(Model):
+    redactor = ForeignKey(
+        NoteUser,
+        on_delete=CASCADE,
+        related_name="notes"
+    )
+    
+
