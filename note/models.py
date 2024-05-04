ee1c1fe2ee7b6849e080f06d635585263c71cf55
ken-morel <engonken8@gmail.com>
2024-05-04 22:53:09 +0100

    Update models.py

--- note/models.py
+++ note/models.py
@@ -1,12 +1,18 @@
-from django.db.models import *
+from django.db import models
 
+import sbook.models
 # Create your models here.
 
 
-class NoteUser(Model):
-    pass
+class NoteUser(models.Model):
+    sbookAccount = models.ForeignKey(
+        sbook.models.User,
+        related_name="noteAccount",
+        on_delete=models.CASCADE,
+    )
     
 class Note(Model):
+    title = models.CharField(max_length=255)
     redactor = ForeignKey(
         NoteUser,
         on_delete=CASCADE,
