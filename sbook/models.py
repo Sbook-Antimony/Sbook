58629b574f555c51ce9462e333c8fc75e871a893
ken-morel <engonken8@gmail.com>
2024-05-04 22:42:27 +0100

    Update models.py

--- sbook/models.py
+++ sbook/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 import chatty.models as chatty
+
 class User(models.Model):
     name = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     email = models.EmailField()
 
-    chatty = models.ForeignKey(chatty.ChattyUser, related_name="sbook", on_delete=models.CASCADE)
     def __str__(self):
         return f"{self.name}:{self.email}"
