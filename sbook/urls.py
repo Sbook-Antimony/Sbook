9d81d1e40a9c3fd3d0739e55469bfb2c6d95e029
ken-morel <engonken8@gmail.com>
2024-05-04 12:25:06 +0100

    added import re_path in sbook.urls

--- sbook/urls.py
+++ sbook/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_pathadded re_path, include
 from . import views
 urlpatterns = [
     re_path('djga/', include('google_analytics.urls')),
