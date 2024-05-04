b97ce356d56b685e75ef68c0de2c21efa4b7afc5
ken-morel <engonken8@gmail.com>
2024-05-04 22:27:31 +0100

    Update admin.py

--- chatty/admin.py
+++ chatty/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 
 class ChattyUserAdmin(admin.ModelAdmin):
-    list_display = ("name", "email", "password",)
+    list_display = ('sbookAccount', 'id', 'rooms',)
 
 
 # Register your models here.
