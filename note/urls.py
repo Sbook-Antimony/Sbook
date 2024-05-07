03886a88fc6ea7992097d137d0c807d68a036284
ken-morel <engonken8@gmail.com>
2024-05-07 19:57:32 +0000

    ddd

--- note/urls.py
+++ note/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.do_index),
-    path('header.html', views.do_header),
-    path('my-notes/get-notes.json', views.do_mynotes.do_get_json),
-    path('my-notes/upload', views.do_mynotes.do_upload),
-    path('my-notes/<noteName>/icon.png', views.do_mynotes.do_get_icon),
+    #path('my-notes/get-notes.json', views.do_mynotes.do_get_json),
+    #path('my-notes/upload', views.do_mynotes.do_upload),
+    #path('my-notes/<noteName>/icon.png', views.do_mynotes.do_get_icon),
 ]
