2d62526b47f87a4c0cc4bf6399ff76a9eadd0495
ken-morel <engonken8@gmail.com>
2024-05-07 04:16:43 -0700

    edited script.js added Intersection observer

--- staticfiles/scripts/response.js
+++ staticfiles/scripts/response.js
@@ -1,8 +1,13 @@
 
 var hoverCls = document.querySelectorAll("[hover-class=*]");
 document.querySelectorAll("[hover-class=*]").forEach((element) => {
-    element.addEventListener("hover", function() {
+    element.addEventListener("mouseenter", function() {
         element.classList.add(
+            ...element.getAttribute("hover-class").split(" ")
+        );
+    });
+    element.addEventListener("mouseleave", function() {
+        element.classList.remove(
             ...element.getAttribute("hover-class").split(" ")
         );
     });
@@ -10,14 +15,32 @@ document.querySelectorAll("[hover-class=*]").forEach((element) => {
 
 var viewCls = document.querySelectorAll("[view-class=*]");
 document.querySelectorAll("[view-class=*]").forEach((element) => {
-    element.addEventListener("mouseenter", function() {
-        element.classList.add(
-            ...element.getAttribute("view-class").split(" ")
-        );
+      
+    let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+            // Each entry describes an intersection change for one observed
+            // target element:
+            //   entry.boundingClientRect
+            //   entry.intersectionRatio
+            //   entry.intersectionRect
+            //   entry.isIntersecting
+            //   entry.rootBounds
+            //   entry.target
+            //   entry.time
+            if(entry.isIntersecting) {
+                element.classList.add(
+                    ...element.getAttribute("hover-class").split(" ")
+                );
+            } else {
+                element.classList.remove(
+                    ...element.getAttribute("hover-class").split(" ")
+                );
+            }
+        },
+    {
+        root: null,
+        rootMargin: "0px",
+        threshold: 1.0,
     });
-    element.addEventListener("mouseleave", function() {
-        element.classList.remove(
-            ...element.getAttribute("view-class").split(" ")
-        );
-    });
+    observer.observe(element);
 });
