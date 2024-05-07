9d1d0dc28e5791037afff3996251da2cbae2c8d5
ken-morel <engonken8@gmail.com>
2024-05-07 03:58:20 -0700

    rjry

--- staticfiles/scripts/response.js
+++ staticfiles/scripts/response.js
@@ -1,1 +1,23 @@
-d
+
+var hoverCls = document.querySelectorAll("[hover-class=*]");
+document.querySelectorAll("[hover-class=*]").forEach((element) => {
+    element.addEventListener("hover", function() {
+        element.classList.add(
+            ...element.getAttribute("hover-class").split(" ")
+        );
+    });
+});
+
+var viewCls = document.querySelectorAll("[view-class=*]");
+document.querySelectorAll("[view-class=*]").forEach((element) => {
+    element.addEventListener("mouseenter", function() {
+        element.classList.add(
+            ...element.getAttribute("view-class").split(" ")
+        );
+    });
+    element.addEventListener("mouseleave", function() {
+        element.classList.remove(
+            ...element.getAttribute("view-class").split(" ")
+        );
+    });
+});
