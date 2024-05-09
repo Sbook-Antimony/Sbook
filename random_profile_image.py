0bc4b48ef937a5e0c88da019afcb5b5968eebafd
ken-morel <engonken8@gmail.com>
2024-05-09 14:11:50 +0100

    Update random_profile_image.py

--- random_profile_image.py
+++ random_profile_image.py
@@ -1,32 +1,47 @@
 from PIL import Image, ImageDraw, ImageFont
 import random
 
+eqs = [
+    lambda x, y: x%y,
+    lambda x, y: y%x,
+    lambda x, y: x*y,
+    lambda x, y: x**2//y,
+    lambda x, y: y**2//x,
+    lambda x, y: x+y,
+    lambda x, y: x-y,
+    lambda x, y: y-x,
+    lambda x, y: x**2-y,
+    lambda x, y: y**2-x,
+    lambda x, y: min(x, y),
+    lambda x, y: max(x, y),
+    lambda x, y: x,
+    lambda x, y: y,
+]
+
 def random_profile(size=500):
-    # Create a new image with white background
     image = Image.new("RGB", (size, size), "white")
-    draw = ImageDraw.Draw(image)
-
-    # Define the limited color palette
-    colors = [(229, 43, 80), (0, 255, 255), (255, 171, 129)]  # Antimony, Cyan, Napples
-
-    # Draw random circles
-    for _ in range(random.randint(1, 3)):
-        x = random.randint(0, size)
-        y = random.randint(0, size)
-        radius = random.randint(size // 8, size // 3)
-        color = random.choice(colors)
-        draw.ellipse([(x - radius, y - radius), (x + radius, y + radius)], fill=color)
-
-    # Draw random squares
-    for _ in range(random.randint(1, 3)):
-        x1 = random.randint(0, size)
-        y1 = random.randint(0, size)
-        min_size = size // 5
-        max_size = size // 2
-        width = random.randint(min_size, max_size)
-        height = random.randint(min_size, max_size)
-        color = random.choice(colors)
-        draw.rectangle([(x1, y1), (x1 + width, y1 + height)], fill=color)
-
-    # Save the image
+    s = 1
+    for _ in range(random.randint(3, 10)):
+        print("m")
+        f = random.choice(eqs)
+        print("eee", eqs.index(f))
+        b = (1, 1)#(random.randint(0, size//2), random.randint(0, size//2))
+        e = (size, size)#(random.randint(size-size//2, size), random.randint(size-size//2, size))
+        
+        c = random.randint(0, 2)
+        
+        cen = size // 2
+        for x in range(b[0]-cen, e[0]-cen, s):
+            for y in range(b[1]-cen, e[1]-cen, s):
+                if x*y == 0:
+                    continue
+                p = list(image.getpixel((x+cen, y+cen)))
+                if sum(p) < 500:
+                    continue
+                fx = f(x, y)
+                p[c] = fx % 256
+                image.putpixel((x, y), tuple(p))
+        s += 1
+        s %= 3
+        s += 1
     return image
