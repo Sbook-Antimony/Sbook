3a0ed596b408a3e564788e67939184bacec9de7f
ken-morel <engonken8@gmail.com>
2024-05-10 23:12:16 +0100

    Update accounts.py

--- note/accounts.py
+++ note/accounts.py
@@ -67,92 +67,36 @@ class NoteUser():
         return [ChattyRoom(x) for x in self.model.rooms.all()]
 
 
-class ChattyRoomDoesNotExistError(ValueError):
+class NoteDoesNotExistError(ValueError):
     pass
-class ChattyRoomDoesExistError(ValueError):
+class NoteDoesExistError(ValueError):
     pass
-class ChattyRoom:
+class Note:
     @classmethod
     def from_id(cls, id):
         try:
-            found = models.ChattyRoom.objects.get(id=id)
-        except models.ChattyRoom.DoesNotExist as e:
-            raise ChattyRoomDoesNotExistError() from e
+            found = models.Note.objects.get(id=id)
+        except models.Note.DoesNotExist as e:
+            raise NoteDoesNotExistError() from e
         else:
             return cls(found)
-    @classmethod
-    def create_from_login(cls, user, name):
-        try:
-            obj = models.ChattyRoom(
-                name=name,
-                admin=user,
-                members=[user]
-            )
-            obj.save()
-        except models.ChattyUser.DoesNotExist as e:
-            raise ChattyUserDoesExistError() from e
-        else:
-            return cls(obj)
+
     def __init__(self, model=None):
         if model is None:
-            raise ChattyRoomDoesNotExistError()
+            raise NoteDoesNotExistError()
         self.model = model
+        self.directory = DIR / "notes" / str(model.id)
 
     @functools.cached_property
     def id(self):
         return self.model.id
     @functools.cached_property
-    def name(self):
-        return self.model.name
+    def title(self):
+        return self.model.title
     @functools.cached_property
-    def members(self):
-        return tuple(map(ChattyUser ,self.model.members.all()))
-    @functools.cached_property
-    def admin(self):
-        return ChattyUser(self.model.admin)
-    @functools.cached_property
-    def messages(self):
-        return tuple(map(ChattyMessage, self.model.messages.all()))
+    def redactors(self):
+        return tuple(map(NoteUser, self.model.redactors.all()))
 
-
-class ChattyMessageDoesNotExistError(ValueError):
-    pass
-class ChattyMessagrDoesExistError(ValueError):
-    pass
-class ChattyMessage:
-    @classmethod
-    def create(cls, user, content, room):
-        msg = models.ChattyTextMessage(content=content, sender=user.model, room=room.model)
-        msg.save()
-        return cls(msg)
-    @classmethod
-    def from_id(cls, id):
-        try:
-            found = models.ChattyTextMessage.objects.get(id=id)
-        except models.ChattyTextMessage.DoesNotExist as e:
-            raise ChattyMessageDoesNotExistError() from e
-        else:
-            return cls(found)
-    def __init__(self, model=None):
-        if model is None:
-            raise ChattyMessageDoesNotExistError()
-        self.model = model
-
-    @functools.cached_property
-    def id(self):
-        return self.model.id
-    @functools.cached_property
-    def sender(self):
-        return ChattyUser(self.model.sender)
-    @functools.cached_property
-    def content(self):
-        return self.model.content
-    @functools.cached_property
-    def room(self):
-        return ChattyRoom(self.model.room)
-    @functools.cached_property
-    def sent_date(self):
-        return (self.model.sent_date)
 
 def check_login(func, redirect=True):
     if isinstance(func, bool):
