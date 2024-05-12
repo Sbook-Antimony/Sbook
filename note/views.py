0ca96698ff8fc6906c265c55f800a50330409c6b
ken-morel <engonken8@gmail.com>
2024-05-12 19:16:12 +0100

    ddd

--- note/views.py
+++ note/views.py
@@ -25,21 +25,3 @@ def do_index(req, user):
     )
 
 
-class do_mynotes:
-    def do_get_json(req):
-        ret = []
-        user = accounts.Account(req.session['user-id'])
-        notes_dir = user.folder / 'notes'
-
-        if notes_dir.exists():
-            for note in notes_dir.glob('*'):
-                note = Note(note)
-                ret.append(note.data)
-        return HttpResponse(json.dumps(ret))
-    def do_get_icon(req, noteName):
-        user = accounts.Account(req.session['user-id'])
-        note = Note(user.folder / 'notes' / noteName)
-        return HttpResponse(note.iconFile.read_bytes())
-
-    def do_upload(req):
-        return HttpResponse(plain("upload.html"))
