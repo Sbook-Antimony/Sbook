642b4c1850d07b5379efe62f4393e95bd0d53229
ken-morel <engonken8@gmail.com>
2024-05-12 21:08:47 +0100

    added new antimony logo

--- note/views.py
+++ note/views.py
@@ -11,7 +11,6 @@ from .accounts import *
 @check_login(True)
 def do_index(req, user):
     #print(user, user.sbookAccount, user.sbookAccount.name, user.notes, type(user.notes))
-    
     return HttpResponse(
         render(
             req,
