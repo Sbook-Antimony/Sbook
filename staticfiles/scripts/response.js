160e6390cc41226f8dbb62461b1b1c772fcdbc1a
ken-morel <engonken8@gmail.com>
2024-05-12 21:53:47 +0100

    corrected and included response.js

--- staticfiles/scripts/response.js
+++ staticfiles/scripts/response.js
@@ -15,32 +15,33 @@ document.querySelectorAll("[hover-class=*]").forEach((element) => {
 
 var viewCls = document.querySelectorAll("[view-class=*]");
 document.querySelectorAll("[view-class=*]").forEach((element) => {
-      
-    let observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach((entry) => {
-            // Each entry describes an intersection change for one observed
-            // target element:
-            //   entry.boundingClientRect
-            //   entry.intersectionRatio
-            //   entry.intersectionRect
-            //   entry.isIntersecting
-            //   entry.rootBounds
-            //   entry.target
-            //   entry.time
-            if(entry.isIntersecting) {
-                element.classList.add(
-                    ...element.getAttribute("hover-class").split(" ")
-                );
-            } else {
-                element.classList.remove(
-                    ...element.getAttribute("hover-class").split(" ")
-                );
-            }
+    new IntersectionObserver(
+        (entries, observer) => {
+            entries.forEach((entry) => {
+                // Each entry describes an intersection change for one observed
+                // target element:
+                //   entry.boundingClientRect
+                //   entry.intersectionRatio
+                //   entry.intersectionRect
+                //   entry.isIntersecting
+                //   entry.rootBounds
+                //   entry.target
+                //   entry.time
+                if(entry.isIntersecting) {
+                    element.classList.add(
+                        ...element.getAttribute("hover-class").split(" ")
+                    );
+                } else {
+                    element.classList.remove(
+                        ...element.getAttribute("hover-class").split(" ")
+                    );
+                }
+            });
         },
-    {
-        root: null,
-        rootMargin: "0px",
-        threshold: 1.0,
-    });
-    observer.observe(element);
+        {
+            root: null,
+            rootMargin: "0px",
+            threshold: 1.0,
+        },
+    ).observe(element);
 });
