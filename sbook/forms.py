22acdc58f56a35187fdadf3498fbc81977356753
ken-morel <engonken8@gmail.com>
2024-05-14 00:18:12 +0100

    Update forms.py

--- sbook/forms.py
+++ sbook/forms.py
@@ -8,3 +8,4 @@ class SignupForm(forms.Form):
     name = forms.CharField(label="name")
     email = forms.EmailField(label="email")
     password = forms.CharField(label="password")
+    g_recaptcha_response = forms.CharField(label='g-recaptcha-response'))
