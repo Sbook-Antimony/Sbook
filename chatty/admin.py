369d5f6b4c7b83688ba24037d808eca7e7958b05
ken-morel <engonken8@gmail.com>
2024-05-13 23:47:02 +0100

    Update admin.py removed ChattyUserAdmin

--- chatty/admin.py
+++ chatty/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
 from .models import *
 
-class ChattyUserAdmin(admin.ModelAdmin):
-    list_display = ('sbookAccount', 'id', 'rooms',)
-
-
-# Register your models here.
-admin.site.register(ChattyUser, ChattyUserAdmin)
 admin.site.register(ChattyRoom)
 admin.site.register(ChattyTextMessage)
