6016c4e7c30aeccb92d37bf105b8777bdf95dd26
ken-morel <engonken8@gmail.com>
2024-05-18 16:40:43 +0100

    create classroom

---
+++ classroom/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class (object):
+	"""docstring for """
+	def __init__(self, arg):
+		super(, self).__init__()
+		self.arg = arg
+		
--- sbook/models.py
+++ sbook/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
+import classroom.models as classroom
+
 class User(models.Model):
     name = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     email = models.EmailField()
 
+
     def __str__(self):
         return f"{self.name}:{self.email}"
+
+
+        
