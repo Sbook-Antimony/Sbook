fa78974242ec74352baf682cbf2b1fe066ddafab
ken-morel <engonken8@gmail.com>
2024-05-20 14:46:17 +0100

    Update models.py

--- sbook/models.py
+++ sbook/models.py
@@ -6,10 +6,29 @@ class User(models.Model):
     name = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     email = models.EmailField()
-
-
+    bio = models.models.CharField(max_length=1023)
+    
     def __str__(self):
         return f"{self.name}:{self.email}"
-
+    profile = models.ImageField()
+class Serie(models.Model):
+    levels = models.ManyToManyField(
+        'Level',
+        related_name="series",
+    )
+    profile = models.ImageField()
+class Level(models.Model):
+    position = models.IntegerField()
+    name = models.CharField(max_length=32)
+    profile = models.ImageField()
+    
+class Course(models.Model):
+    name = models.CharField(max_length=32)
+    profile = models.ImageField()
+    levels = models.ManyToManyField(
+        Level,
+        related_name="courses",
+    
+    )    
 
         
