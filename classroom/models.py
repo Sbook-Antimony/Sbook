262c2aa00f01148ef03fd8e596a91b74fcbd906c
ken-morel <engonken8@gmail.com>
2024-05-20 17:50:45 +0100

    Update models.py

--- classroom/models.py
+++ classroom/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
+import sbook.models as sbook
+
 # Create your models here.
 
-class Classroom(object):
-    """docstring for """
-    name = models.CharField(max_length=255)		
+class Classroom(models.Model)
+    name = models.CharField(max_length=255)
+    profile = models.ImageField()
+    description = models.CharField(max_length=255)
+    levels = models.ManyToManyField(
+        sbook.Level,
+        related_name='classrooms',
+    )
+    courses = models.ManyToManyField(
+        sbook.Level,
+        related_name='classrooms',
+    )
