7c3cec871e8afdac66beb78e19da815553fad674
ken-morel <engonken8@gmail.com>
2024-05-31 00:21:16 +0100

    Dev (#22)
    * Update urls.py
    
    * Update quizz-attempt-review.django
    
    * Update urls.py
    
    * Update views.py
    
    * Update views.py
    
    * Update models.py

--- quizz/models.py
+++ quizz/models.py
@@ -29,6 +29,7 @@ class QuizzAttempt(models.Model):
     answers = models.JSONField()
     remark = models.JSONField(default=None, null=True)
     remarked = models.BooleanField(default=False)
+    score = models.IntegerField(default=0)
 
     def __str__(self):
         return f'AnswerAttempt({self.id}:{self.remarked})'
